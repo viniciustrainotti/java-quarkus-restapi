674067a2f061d106fc8feb14cfcf80fd9560bda0
Vinícius Trainotti <12179459+viniciustrainotti@users.noreply.github.com>
2024-03-03 21:42:22 -0300

    feat: initial commit

---
+++ src/main/java/org/acme/dto/Product.java
@@ -0,0 +1,15 @@
+package src.main.java.org.acme.dto;
+
+public class Product {
+
+    public Long id;
+
+    public String label;
+
+    public String description;
+
+    public Integer quantity;
+    
+    public Double price;
+
+}
